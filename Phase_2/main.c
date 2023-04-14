0c3b4878e88629a5f732db3b3bb56ef4d27e076b
AleBombi <alessandroravveduto@gmail.com>
2023-04-14 13:24:06 +0200

    continuo inizializzazione

--- Phase_2/main.c
+++ Phase_2/main.c
@@ -1,4 +1,5 @@
 #include <umps3/umps/libumps.h>
+#include <umps3/umps/types.h>
 #include <pcb.h>
 #include <ash.h>
 #include <ns.h>
@@ -9,6 +10,7 @@ int soft_block_count; /* conteggio processi bloccati per I/O o timer request*/
 LIST_HEAD(readyQ); /* lista processi ready */
 pcb_t* current_process; /* puntatore al processo in esecuzione */
 
+int sem_processor_local_timer;
 int sem_interval_timer;
 int sem_disk[8];
 int sem_tape[8];
@@ -16,6 +18,12 @@ int sem_network[8];
 int sem_printer[8];
 int sem_terminal[16];
 
+void uTLB_RefillHandler () {
+    setENTRYHI(0x80000000);
+    setENTRYLO(0x00000000);
+    TLBWR();
+    LDST ((state_PTR) 0x0FFFF000);
+}
 
 int main() {
 
@@ -31,6 +39,7 @@ int main() {
     current_process = NULL;
 
 /* inizializzazione semafori */
+    sem_processor_local_timer=0;
     sem_interval_timer=0;
 
     for( int i=0; i<8; i++) {
@@ -43,6 +52,18 @@ int main() {
     }
 
 /* popolazione pass up vector */
-    uTLB_RefillHandler();
-    setTIMER(100);
+    passupvector_t *passUpVect;
+    passUpVect->tlb_refill_handler = (memaddr) uTLB_RefillHandler;
+    passUpVect->tlb_refill_stackPtr = (memaddr) 0x20001000;
+    passUpVect->exception_handler = (memaddr) FUNZIONE_DA_IMPLEMENTARE;
+    passUpVect->exception_stackPtr = (memaddr) 0x20001000; //stesso indirizzo ??
+
+    //PUNTO 5 NON HO CAPITO
+    setTIMER(100); //non è giusto usare questo penso
+
+    //PUNTO 6 
+    pcb_t *primoProc = allocPcb();
+    insertProcQ(&readyQ, &primoProc);
+    process_count+=1;
+    //mancano i file del tutor
 }
