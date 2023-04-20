ee1de54c2026e31f3d45eb07d528232d4538055c
AleBombi <alessandroravveduto@gmail.com>
2023-04-20 16:13:40 +0200

    aggiunta file per segnare le cose e adderbuf

--- Phase_2/p2test.01.c
+++ Phase_2/p2test.01.c
@@ -141,6 +141,109 @@ void uTLB_RefillHandler() {
     LDST((state_t *)0x0FFFF000);
 }
 
+/* AGGIUNTA ADDERRBUF PER AIUTARE IL DEBUG, USARE COME UNA PRINT */
+
+char   okbuf[2048]; /* sequence of progress messages */
+char   errbuf[128]; /* contains reason for failing */
+char   msgbuf[128]; /* nonrecoverable error message before shut down */
+int    sem[MAXSEM];
+int    onesem;
+pcb_t *procp[MAXPROC], *p, *q, *firstproc, *lastproc, *midproc;
+nsd_t *pid_ns, *pid_ns2;
+char  *mp = okbuf;
+
+
+#define TRANSMITTED 5
+#define ACK         1
+#define PRINTCHR    2
+#define CHAROFFSET  8
+#define STATUSMASK  0xFF
+#define TERM0ADDR   0x10000254
+
+typedef unsigned int devreg;
+
+/* This function returns the terminal transmitter status value given its address */
+devreg termstat(memaddr *stataddr) {
+    return ((*stataddr) & STATUSMASK);
+}
+
+/* This function prints a string on specified terminal and returns TRUE if
+ * print was successful, FALSE if not   */
+unsigned int termprint(char *str, unsigned int term) {
+    memaddr     *statusp;
+    memaddr     *commandp;
+    devreg       stat;
+    devreg       cmd;
+    unsigned int error = FALSE;
+
+    if (term < DEVPERINT) {
+        /* terminal is correct */
+        /* compute device register field addresses */
+        statusp  = (devreg *)(TERM0ADDR + (term * DEVREGSIZE) + (TRANSTATUS * DEVREGLEN));
+        commandp = (devreg *)(TERM0ADDR + (term * DEVREGSIZE) + (TRANCOMMAND * DEVREGLEN));
+
+        /* test device status */
+        stat = termstat(statusp);
+        if (stat == READY || stat == TRANSMITTED) {
+            /* device is available */
+
+            /* print cycle */
+            while (*str != EOS && !error) {
+                cmd       = (*str << CHAROFFSET) | PRINTCHR;
+                *commandp = cmd;
+
+                /* busy waiting */
+                stat = termstat(statusp);
+                while (stat == BUSY)
+                    stat = termstat(statusp);
+
+                /* end of wait */
+                if (stat != TRANSMITTED)
+                    error = TRUE;
+                else
+                    /* move to next char */
+                    str++;
+            }
+        } else
+            /* device is not available */
+            error = TRUE;
+    } else
+        /* wrong terminal device number */
+        error = TRUE;
+
+    return (!error);
+}
+
+
+/* This function placess the specified character string in okbuf and
+ *	causes the string to be written out to terminal0 */
+void addokbuf(char *strp) {
+    char *tstrp = strp;
+    while ((*mp++ = *strp++) != '\0')
+        ;
+    mp--;
+    termprint(tstrp, 0);
+}
+
+
+/* This function placess the specified character string in errbuf and
+ *	causes the string to be written out to terminal0.  After this is done
+ *	the system shuts down with a panic message */
+void adderrbuf(char *strp) {
+    char *ep    = errbuf;
+    char *tstrp = strp;
+
+    while ((*ep++ = *strp++) != '\0')
+        ;
+
+    termprint(tstrp, 0);
+
+    PANIC();
+}
+
+/* ------- COSE DA RIMUOVERE ------- */
+
+
 
 /*********************************************************************/
 /*                                                                   */
