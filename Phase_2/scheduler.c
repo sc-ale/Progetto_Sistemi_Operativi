df97c7a517e7425044a21c719c56d735b4094685
AleBombi <alessandroravveduto@gmail.com>
2023-06-17 19:15:58 +0200

    diocan

--- Phase_2/scheduler.c
+++ Phase_2/scheduler.c
@@ -4,37 +4,34 @@
 #include "scheduler.h"
 
 void scheduling(){
-    while (true) {
-        /* readyQ non vuota */
-        if (!emptyProcQ(&readyQ))
-        {
-            current_process = removeProcQ(&readyQ);
-            setTIMER(TIMESLICE);
+    /* readyQ non vuota */
+    if (!emptyProcQ(&readyQ))
+    {
+        current_process = removeProcQ(&readyQ);
+        setTIMER(TIMESLICE);
 
-            STCK(current_process->istante_Lancio_Blocco); 
-            /* per salvarsi il momento in cui viene lanciato */
-            
-            LDST((STATE_PTR)&current_process->p_s);
-        }
-        else if(process_count==0)
-        {
-            HALT();
-        }
-        else if(process_count>0 && soft_block_count>0)
-        {
-            /* the Scheduler must first set the Status register
-             to enable interrupts and either disable the PLT
-            The first interrupt that occurs after entering a 
-            Wait State should not be for the PLT. */
-            setSTATUS((getSTATUS() ^ TEBITON) | IEPON);
-            /* fare qualcosa con il PLT*/
-            WAIT();
-        }
-        else if (process_count>0 && soft_block_count==0)
-        {
-            PANIC();
-        }
+        STCK(current_process->istante_Lancio_Blocco); 
+        /* per salvarsi il momento in cui viene lanciato */
         
+        LDST((STATE_PTR)&current_process->p_s);
+    }
+    else if(process_count==0)
+    {
+        HALT();
+    }
+    else if(process_count>0 && soft_block_count>0)
+    {
+        /* the Scheduler must first set the Status register
+            to enable interrupts and either disable the PLT
+        The first interrupt that occurs after entering a 
+        Wait State should not be for the PLT. */
+        setSTATUS((getSTATUS() ^ TEBITON) | IEPON);
+        /* fare qualcosa con il PLT*/
+        WAIT();
+    }
+    else if (process_count>0 && soft_block_count==0)
+    {
+        PANIC();
     }
 }
 #endif
